9454bd59512d01a55570b845838c13e47b0c81fa
Bryan Mwas <brian.wangome@bosch.com>
2025-04-22 15:01:21 +0300

    Added runtime listener to enable tooltip display

--- quick-api-reference/sw-tips.js
+++ quick-api-reference/sw-tips.js
@@ -25,3 +25,10 @@ async function createAlarm() {
 createAlarm();
 
 chrome.alarms.onAlarm.addListener(updateTip);
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting == "tip") {
+    chrome.storage.local.get("tip").then(sendResponse);
+    return true;
+  }
+});
